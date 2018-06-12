7f782c3820225ac07fcd5b970be27f9c1aa35e67
Eric M Todd <ericmtodd@Erics-MacBook-Air.local>
2018-06-12 15:53:45 -0600

    Commit 2 - The 2nd set of GIT homework answers are complete.

--- js/app.js
+++ js/app.js
@@ -5,3 +5,13 @@ console.log("JavaScript is running...")
 //1. git init
 //2. git add .
 //3. git commit -m "version message here"
+
+//1ST COMMIT MADE
+
+//1. git pull origin master
+//2. git reset <filePath>
+//3. git reset --soft HEAD^
+//4. it separates a branch names from a filename
+//5. incase you accidentally commit something you didn't want committed
+
+//2ND COMMIT
