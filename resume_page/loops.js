d39e211c7b5f28067aa07af860bf18d8de410415
Eric M Todd <ericmtodd@Erics-MacBook-Air.local>
2018-06-12 18:06:13 -0600

    Commit 9 - Created loops.js file.

---
+++ resume_page/loops.js
@@ -0,0 +1,36 @@
+console.log("JavaScript is up and running!")
+
+//loop that logs numbers 0-99 ascending.
+for (num1 = 0; num1 <= 99; num1++) {
+	console.log(num1);
+};
+
+//loop that logs numbers 99-0 descending.
+for (num2 = 99; num2 >= 0; num2--) {
+	console.log(num2);
+};
+
+//loop that logs even numbers, 0 - 98 ascending.
+ for (num3 = 0; num3 <= 98; num3+=2) {
+ 	console.log(num3);
+};
+
+//loop that logs even numbers, 98 - 0 descending.
+for (num4 = 98; num4 >= 0; num4-=2) {
+	console.log(num4);
+};
+
+//loop that logs numbers 49 - 72 ascending.
+for (num5 = 49; num5 <= 72; num5++) {
+	console.log(num5);
+};
+
+//loop that logs numbers 81 - 26 descending.
+for (num6 = 81; num6 >= 26; num6--) {
+	console.log(num6);
+};
+
+//loop that logs numbers 3 - 90 ascending by 3.
+for (num7 = 3; num7 <= 90; num7+=3) {
+	console.log(num7);
+};
